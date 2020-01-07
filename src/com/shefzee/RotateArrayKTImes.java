c19d7bd4b4a790a7be0dec35af9f3aa3c2aaeb39
Shefeeque Mohammed <34381191+shefeequemohammed@users.noreply.github.com>
2020-01-07 21:01:27 +0400

    Update RotateArrayKTImes.java

--- src/com/shefzee/RotateArrayKTImes.java
+++ src/com/shefzee/RotateArrayKTImes.java
@@ -1,32 +1,21 @@
 package com.shefzee;
-
 import java.util.Arrays;
-
 public class RotateArrayKTImes {
 
     static void rotate(int[] arr, int k){
 
         for(int i=0; i< k; i++ ){
-
             int temp = arr[0];
-
             for(int j=0; j< arr.length -1; j++){
-
                 arr[j] = arr[j+1];
-
             }
-
             arr[arr.length -1] = temp;
         }
-
         Arrays.stream(arr).forEach(System.out::println);
     }
-
-
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5};
-
         rotate(arr,2);
     }
 }
